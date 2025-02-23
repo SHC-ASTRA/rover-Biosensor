f419f33579745c0961a9d6d3d5ef4f59300f8cd4
David Sharpe <ds0196@uah.edu>
2025-02-22 21:55:29 -0600

    feat: begin motor mcu code

---
+++ citadel_motor/src/main.cpp
@@ -0,0 +1,196 @@
+/**
+ * @file main.cpp
+ * @author David Sharpe (ds0196@uah.edu)
+ * @brief Citadel's secondary microcontroller; controls 4 stepper motors
+ *
+ */
+
+//------------//
+//  Includes  //
+//------------//
+
+#include "AstraMisc.h"
+#include "project/CITADEL.h"
+
+
+//------------//
+//  Settings  //
+//------------//
+
+// Comment out to disable LED blinking
+#define BLINK
+
+#define COMMS_UART Serial  // Serial1 to talk to main mcu
+
+
+//---------------------//
+//  Component classes  //
+//---------------------//
+
+
+//----------//
+//  Timing  //
+//----------//
+
+uint32_t lastBlink = 0;
+bool ledState = false;
+
+
+//--------------//
+//  Prototypes  //
+//--------------//
+
+
+//------------------------------------------------------------------------------------------------//
+//  Setup
+//------------------------------------------------------------------------------------------------//
+//
+//
+//------------------------------------------------//
+//                                                //
+//      ////////    //////////    //////////      //
+//    //                //        //        //    //
+//    //                //        //        //    //
+//      //////          //        //////////      //
+//            //        //        //              //
+//            //        //        //              //
+//    ////////          //        //              //
+//                                                //
+//------------------------------------------------//
+void setup() {
+    //--------//
+    //  Pins  //
+    //--------//
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+
+
+    //------------------//
+    //  Communications  //
+    //------------------//
+
+    Serial.begin(SERIAL_BAUD);
+
+
+    //-----------//
+    //  Sensors  //
+    //-----------//
+
+
+    //--------------------//
+    //  Misc. Components  //
+    //--------------------//
+}
+
+
+//------------------------------------------------------------------------------------------------//
+//  Loop
+//------------------------------------------------------------------------------------------------//
+//
+//
+//-------------------------------------------------//
+//                                                 //
+//    /////////      //            //////////      //
+//    //      //     //            //        //    //
+//    //      //     //            //        //    //
+//    ////////       //            //////////      //
+//    //      //     //            //              //
+//    //       //    //            //              //
+//    /////////      //////////    //              //
+//                                                 //
+//-------------------------------------------------//
+void loop() {
+    //----------//
+    //  Timers  //
+    //----------//
+#ifdef BLINK
+    if (millis() - lastBlink > 1000) {
+        lastBlink = millis();
+        ledState = !ledState;
+        digitalWrite(LED_BUILTIN, ledState);
+    }
+#endif
+
+
+    //-------------//
+    //  CAN Input  //
+    //-------------//
+
+
+    //------------------//
+    //  UART/USB Input  //
+    //------------------//
+    //
+    //
+    //-------------------------------------------------------//
+    //                                                       //
+    //      /////////    //\\        ////    //////////      //
+    //    //             //  \\    //  //    //        //    //
+    //    //             //    \\//    //    //        //    //
+    //    //             //            //    //        //    //
+    //    //             //            //    //        //    //
+    //    //             //            //    //        //    //
+    //      /////////    //            //    //////////      //
+    //                                                       //
+    //-------------------------------------------------------//
+    if (Serial.available()) {
+        String input = Serial.readStringUntil('\n');
+
+        input.trim();                   // Remove preceding and trailing whitespace
+        std::vector<String> args = {};  // Initialize empty vector to hold separated arguments
+        parseInput(input, args, ',');   // Separate `input` by commas and place into args vector
+        args[0].toLowerCase();          // Make command case-insensitive
+        String command = args[0];       // To make processing code more readable
+
+        //--------//
+        //  Misc  //
+        //--------//
+        /**/ if (command == "ping") {
+            Serial.println("pong");
+        }
+
+        else if (command == "time") {
+            Serial.println(millis());
+        }
+
+        else if (command == "led") {
+            if (args[1] == "on")
+                digitalWrite(LED_BUILTIN, HIGH);
+            else if (args[1] == "off")
+                digitalWrite(LED_BUILTIN, LOW);
+            else if (args[1] == "toggle") {
+                ledState = !ledState;
+                digitalWrite(LED_BUILTIN, ledState);
+            }
+        }
+
+        //-----------//
+        //  Sensors  //
+        //-----------//
+
+        //----------//
+        //  Motors  //
+        //----------//
+    }
+}
+
+
+//------------------------------------------------------------------------------------------------//
+//  Function definitions
+//------------------------------------------------------------------------------------------------//
+//
+//
+//----------------------------------------------------//
+//                                                    //
+//    //////////    //          //      //////////    //
+//    //            //\\        //    //              //
+//    //            //  \\      //    //              //
+//    //////        //    \\    //    //              //
+//    //            //      \\  //    //              //
+//    //            //        \\//    //              //
+//    //            //          //      //////////    //
+//                                                    //
+//----------------------------------------------------//
